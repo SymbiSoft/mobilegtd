6e0fbbbddc68dfe08e65d9ab1807a0dcf69794e8
Martin.Mauch <Martin.Mauch@4d0d0bda-e137-0410-9d53-71468b758b3d>
2008-05-31 03:32:27 +0000

    Release 1.0.0 alpha

--- trunk/MobileGTD/default.py
+++ trunk/MobileGTD/default.py
@@ -1,17 +1,6 @@
-import os,re,codecs,sys,appuifw,traceback
-import mobilegtd
-import mobilegtd.gui,mobilegtd.model
-import mobilegtd.config, mobilegtd.defaultconfig
-import mobilegtd.logging
-import mobilegtd.io,mobilegtd.parsing,mobilegtd.keys
-
-
-
-from inbox import EInbox,Inbox
-from e32 import Ao_lock
-from mobilegtd.config import *
-from mobilegtd.logging import logger
-#import appswitch
+# SYMBIAN_UID = 0xA0008CDC
+# 0xA0008CDC
+# 0x2001A1A0
 
 
 
@@ -22,14 +11,6 @@ from mobilegtd.logging import logger
 
 
 
-if not os.path.exists(project_directory):
-    os.makedirs(project_directory)
-
-
-
-
-
-INBOX = Inbox(EInbox)
 def to_unicode():
     return lambda x:x.encode('utf-8')
 
@@ -39,6 +20,37 @@ def to_unicode():
 
 lock=None
 try:
+    import e32
+    import os,re,codecs,sys,appuifw,traceback
+    e32.ao_yield()
+    def display(objects):
+        strings=[]
+        for object in objects:
+            strings.append(u'%s'%object)
+        appuifw.selection_list(strings)
+    from e32 import Ao_lock
+    #appuifw.note(u'Vor Imports')
+    #display(sys.path)
+    sys.path.append('c:/python/')
+
+    import logging
+    from logging import logger
+    
+    import gui,model
+    import config, defaultconfig
+    import io
+    from config import *
+    
+    
+    
+    #import appswitch
+
+    if not os.path.exists(project_directory):
+        os.makedirs(project_directory)
+    
+    
+    
+    
 
 #    logger.log(u'Configuration')
 #    logger.log(repr(COMMON_CONFIG))
@@ -46,8 +58,8 @@ try:
 #    logger.log(repr(ABBREVIATIONS))
 #    logger.log(u'Keys and Menus')
 #    logger.log(repr(ACTION_LIST_KEYS_AND_MENU))
-    projects = mobilegtd.model.Projects(project_directory)
-    projects_view = mobilegtd.gui.ProjectListView(projects)
+    projects = model.Projects(project_directory)
+    projects_view = gui.ProjectListView(projects)
     projects_view.run()
 except Exception, e:
     error_text = unicode(repr(e.args))
@@ -55,14 +67,14 @@ except Exception, e:
     t.add(error_text)
     for trace_line in traceback.extract_tb(sys.exc_info()[2]):
         formatted_trace_line = u'\nIn %s line %s: %s "%s"'%trace_line
-        logger.log(formatted_trace_line,1)
+        #logger.log(formatted_trace_line,1)
         t.add(formatted_trace_line)
-    appuifw.app.menu=[(u'Exit', mobilegtd.gui.exit)]
+    appuifw.app.menu=[(u'Exit', gui.exit)]
 
     appuifw.app.title=u'Error'
     appuifw.app.body=t
     lock = Ao_lock()
-    #appuifw.app.exit_key_handler=mobilegtd.gui.exit
+    #appuifw.app.exit_key_handler=gui.exit
     lock.wait()
 
-logger.close()
+#logger.close()
